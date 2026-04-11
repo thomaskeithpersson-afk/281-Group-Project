23a15f19b534be192570a2dcebd7962324666cc7
David James-Nwokocha <tandiajames10@gmail.com>
2026-04-10 19:38:24 -0600

    Add files via upload

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,40 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const dataPath = path.join(__dirname, '../data/users.json');
-const getUsers = () => {
-    try {
-        const data = fs.readFileSync(dataPath, 'utf-8');
-        if (!data.trim()) {
-            return [];
-        }
-        return JSON.parse(data);
-    } catch (error) {
-        return [];
-    }
-};;
-const saveUsers = (users) => {
-    fs.writeFileSync(dataPath, JSON.stringify(users, null, 2));
-};
-const findUserByUsername = (username) => {
-    const users = getUsers();
-    return users.find(u => u.username === username);
-};
-const createUser = (username, password) => {
-    if (findUserByUsername(username)) return null; 
-    const users = getUsers();
-    const newUser = {
-        id: uuidv4(),
-        username: username,
-        password: password
-    };
-    users.push(newUser);
-    saveUsers(users);
-    return newUser;
-};
-const checkPassword = (username, password) => {
-    const user = findUserByUsername(username);
-    if (!user) return false;
-    return user.password === password; 
-};
-module.exports = { findUserByUsername, createUser, checkPassword };
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: { type: String, required: false }
+});
+const plugin = passportLocalMongoose.default || passportLocalMongoose;
+userSchema.plugin(plugin);
+module.exports = mongoose.model('User', userSchema);
